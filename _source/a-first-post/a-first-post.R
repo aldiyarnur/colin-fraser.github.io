c783df80062318dd49b232aa2c5b15a34f2a7269
colinfraser <colin.r.fraser@gmail.com>
2016-08-29 14:00:16 -0700

    delete some posts

--- _source/a-first-post/a-first-post.R
+++
@@ -1,5 +0,0 @@
-# This R file accomanies the .Rmd blog post
-# _source/a-first-post/2016-08-25-a-first-post.Rmd
-
-library(dplyr)
-library(ggplot2)
--- _source/one-more-post/one-more-post.R
+++
@@ -1,3 +0,0 @@
-# This R file accomanies the .Rmd blog post
-# _source/one-more-post/2016-08-25-one-more-post.Rmd
-
